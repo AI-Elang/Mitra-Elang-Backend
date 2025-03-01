b0be5ba5cf63c97e44001d31d84cd069e7794576
Arkade7 <vedo.priyantoro@gmail.com>
2025-03-01 15:33:13 +0700

    add is_active in dashboard response menu

--- app/Http/Services/Api/V1/Dashboard/DashboardService.php
+++ app/Http/Services/Api/V1/Dashboard/DashboardService.php
@@ -16,9 +16,9 @@ class DashboardService
             ->get();
 
         $parameter = DB::table('parameter_mitra')
-            ->select('id', 'name')
+            ->select('id', 'name', 'is_active')
             ->whereIn('parameter_mitra.id', $parameterRegion->pluck('parameter_id'))
-            ->where('parameter_mitra.is_active', True)
+//            ->where('parameter_mitra.is_active', True)
             ->get();
 
         $subparameter = DB::table('subparameter_mitra')
