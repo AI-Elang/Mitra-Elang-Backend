166545d1cc28cb0c70b5fe04b0550de3b1d6dca7
Arkade7 <vedo.priyantoro@gmail.com>
2025-03-03 21:11:48 +0700

    fix dashboard variable response + fix dashboard template response

--- app/Http/Services/Api/V1/Dashboard/DashboardService.php
+++ app/Http/Services/Api/V1/Dashboard/DashboardService.php
@@ -9,23 +9,12 @@ class DashboardService
 
     public function getRegionId($mcId)
     {
-        $branchId = DB::table('territory_dashboards')
-            ->select('id', 'id_secondary')
-            ->where('id', $mcId)
-            ->first();
-
-        $areaId = DB::table('territory_dashboards')
-            ->select('id_secondary')
-            ->where('id', $branchId->id_secondary)
-            ->first();
-
-        $regionId = DB::table('territory_dashboards')
-            ->select('id_secondary')
-            ->where('id', $areaId->id_secondary)
-            ->first();
-        return $regionId->id_secondary;
+        return DB::table('territory_dashboards as t1')
+            ->join('territory_dashboards as t2', 't1.id_secondary', '=', 't2.id')
+            ->join('territory_dashboards as t3', 't2.id_secondary', '=', 't3.id')
+            ->where('t1.id', $mcId)
+            ->value('t3.id_secondary');
     }
-
     /**
      * Method untuk mengambil id region dari id mc yang diberikan.
      */
@@ -36,15 +25,9 @@ class DashboardService
         $useRole = auth('api')->user()->role_label;
         $regionId = $this->getRegionId($mcId);
 
-        $parameterRegion = DB::table('region_parameter')
-            ->where('is_active', true)
-            ->where('territory_id', $regionId)
-            ->where('type', $useRole)
-            ->pluck('parameter_id')
-            ->toArray();
-
         $parameters = DB::table('parameter_mitra as p')
             ->join('subparameter_mitra as sp', 'p.id', '=', 'sp.parameter_id')
+            ->join('region_parameter as rp', 'p.id', '=', 'rp.parameter_id')
             ->select(
                 'p.id as parameter_id',
                 'p.name as parameter_name',
@@ -54,10 +37,15 @@ class DashboardService
                 'sp.kolom as subparameter_column',
                 'p.tabel as parameter_table',
                 'sp.tabel as subparameter_table',
-                'p.is_active as parameter_is_active',
+                'p.is_active as parameter_is_active'
             )
-            ->whereIn('p.id', $parameterRegion)
+            ->where('rp.is_active', true)
+            ->where('sp.is_active', true)
+            ->where('p.is_active', true)
+            ->where('rp.territory_id', $regionId)
+            ->where('rp.type', $useRole)
             ->get();
+
 
         $data = [];
 
@@ -73,7 +61,13 @@ class DashboardService
                     'id' => $param->parameter_id,
                     'name' => $param->parameter_name,
                     'is_active' => $param->parameter_is_active,
-                    $param->parameter_column => isset($parameterValue->{$param->parameter_column}) ? (float)number_format((float) $parameterValue->{$param->parameter_column}, 1) : "0.0",
+                    'value' => isset($parameterValue->{$param->parameter_column})
+                        ? (is_numeric($parameterValue->{$param->parameter_column})
+                            ? (is_float($floatValue = (float) $parameterValue->{$param->parameter_column})
+                                ? round($floatValue, 1)
+                                : $parameterValue->{$param->parameter_column})
+                            : $parameterValue->{$param->parameter_column})
+                        : 0.0,
                     'subparameters' => []
                 ];
             }
@@ -87,12 +81,15 @@ class DashboardService
                 'id' => $param->subparameter_id,
                 'name' => $param->subparameter_name,
                 'parameter_id' => $param->parameter_id,
-                'tabel' => $param->subparameter_table,
-                'kolom' => $param->subparameter_column,
-                'value' => isset($subparameterValue->{$param->subparameter_column}) ? $subparameterValue->{$param->subparameter_column} : null
+                'value' => isset($subparameterValue->{$param->subparameter_column})
+                    ? (is_numeric($subparameterValue->{$param->subparameter_column})
+                        ? (is_float($floatValue = (float) $subparameterValue->{$param->subparameter_column})
+                            ? round($floatValue, 1)
+                            : $subparameterValue->{$param->subparameter_column})
+                        : $subparameterValue->{$param->subparameter_column})
+                    : 0.0,
             ];
         }
-
         return array_values($data);
     }
 
@@ -100,14 +97,9 @@ class DashboardService
     public function account()
     {
         $username = auth('api')->user()->username;
-        $mcId = auth('api')->user()->territory_id;
-        $mc_name = DB::table('territory_dashboards')
-            ->select('name')
-            ->where('id', $mcId)
-            ->first();
 
         $profile = DB::table('mitra_table')
-            ->select('id_mitra', 'nama_mitra', 'nama_owner')
+            ->select('id_mitra', 'nama_mitra', 'nama_owner', 'type')
             ->where('is_active', True)
             ->where('id_mitra', $username)
             ->get();
